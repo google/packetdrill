c5fd1f02ad37c3e2859b3c07c263d2406e5b6c3c
Davide Caratti <davide.caratti@gmail.com>
2020-01-17 18:37:02 +0100

    fix endianess issue on inbound DSN8/DACK8
    packetdrill fails to convert the DSN8 and the DACK8 to network byte order
    on little endian systems, because htole64(x) and le64toh(x) are a no-op.
    On LE systems, we need to flip DSN and DACK octets before transmitting,
    and leave them untouched on BE (this latter part was probably correctly
    covered by be64toh() and htobe64()).
    
    replace htonll() and ntohll() with another version that correctly flips
    these octets on LE CPUs.
    
    Signed-off-by: Davide Caratti <dcaratti@redhat.com>

--- gtests/net/packetdrill/mptcp.h
+++ gtests/net/packetdrill/mptcp.h
@@ -26,14 +26,8 @@
 #include <linux/hash_info.h>
 
 
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-	#define ntohll(x)  le64toh(x)
-	#define htonll(x)  htole64(x)
-#elif __BYTE_ORDER == __BIG_ENDIAN
-	#define ntohll(x) be64toh(x)
-	#define htonll(x) htobe64(x)
-#endif
-
+#define htonll(x) ((1==htonl(1)) ? (x) : ((uint64_t)htonl((x) & 0xFFFFFFFF) << 32) | htonl((x) >> 32))
+#define ntohll(x) ((1==ntohl(1)) ? (x) : ((uint64_t)ntohl((x) & 0xFFFFFFFF) << 32) | ntohl((x) >> 32))
 
 #define MPTCP_VERSION 0
 
