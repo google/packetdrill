7b1c4c348ed19d69d4426aefadd8611eeb8af4f2
Shuo Chen <shuochen@google.com>
2022-10-28 17:57:39 +0000

    net-test: packetdrill: update upstream tcp/common/set_sysctls.py
    Updates set_sysctls.py to match all changes up to and including internal
    commit SHA1 0c991040df7c5.
    
    Original-by: John Sperbeck
    Signed-off-By: Shuo Chen <shuochen@google.com>

--- gtests/net/tcp/common/set_sysctls.py
+++ gtests/net/tcp/common/set_sysctls.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python
+#
+# Copyright 2012 Google Inc. All Rights Reserved.
 
-import commands
+"""Sets sysctl values and writes a file that restores them.
+
+The arguments are of the form "<proc-file>=<val>" separated by spaces.
+The program first reads the current value of the proc-file and creates
+a shell script named "/tmp/sysctl_restore_${PPID}.sh" which restores the values
+when executed. It then sets the new values
+"""
+
+__author__ = ('brakmo@google.com (Lawrence Brakmo)')
+
 import os
+import subprocess
 import sys
 
 pppid = int(os.popen("ps -p %d -oppid=" % os.getppid()).read().strip())
 filename = '/tmp/sysctl_restore_%d.sh' % pppid
 
+# Open file for restoring sysctl values
 restore_file = open(filename, 'w')
-print >> restore_file, '#!/bin/bash'
+print('#!/bin/bash', file=restore_file)
 
 for a in sys.argv[1:]:
   sysctl = a.split('=')
+  # sysctl[0] contains the proc-file name, sysctl[1] the new value
 
-  # save current value
-  cur_val = commands.getoutput('cat ' + sysctl[0])
-  print >> restore_file, 'echo "%s" > %s' % (cur_val, sysctl[0])
+  # read current value and add restore command to file
+  cur_val = subprocess.check_output(['cat', sysctl[0]], universal_newlines=True)
+  print('echo "%s" > %s' % (cur_val.strip(), sysctl[0]), file=restore_file)
 
   # set new value
   cmd = 'echo "%s" > %s' % (sysctl[1], sysctl[0])
