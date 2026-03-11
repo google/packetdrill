d64fae5095c758e40ff9bc34b214284c4053b3d4
Daniel Zahka <daniel.zahka@gmail.com>
2026-03-11 10:01:45 -0700

    net-test: packetdrill: add ynl_psp library
    This code is adapted from
    tools/testing/selftests/drivers/net/psp_responder.c in the linux
    source tree. It simplifies using the linux PSP uapi.
    
    On the wire_server side we disable psp rx on the NIC, so that we can
    see un-decapsulated, un-decrypted frames.
    
    Signed-off-by: Daniel Zahka <daniel.zahka@gmail.com>

---
+++ gtests/net/packetdrill/ynl_psp.h
@@ -0,0 +1,45 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __YNL_PSP_H__
+#define __YNL_PSP_H__
+
+#include "types.h"
+
+struct ynl_psp_state;
+
+#if defined(__linux__) && defined(ENABLE_PSP)
+
+#include <ynl.h>
+
+struct ynl_psp_state *ynl_psp_new(bool enable_psp_rx, const char *ifname);
+void ynl_psp_free(struct ynl_psp_state *state);
+int ynl_psp_rx_assoc(struct ynl_psp_state *state, int live_sock,
+		     u32 *live_spi);
+int ynl_psp_tx_assoc(struct ynl_psp_state *state, int live_sock, u32 spi);
+
+#else
+
+static inline struct ynl_psp_state *ynl_psp_new(bool enable_psp_rx,
+						 const char *ifname)
+{
+	return NULL;
+}
+
+static inline void ynl_psp_free(struct ynl_psp_state *state)
+{
+}
+
+static inline int ynl_psp_rx_assoc(struct ynl_psp_state *state, int live_sock,
+				   u32 *live_spi)
+{
+	return STATUS_ERR;
+}
+
+static inline int ynl_psp_tx_assoc(struct ynl_psp_state *state, int live_sock,
+				   u32 spi)
+{
+	return STATUS_ERR;
+}
+
+#endif /* defined(__linux__) && defined(ENABLE_PSP) */
+
+#endif /* __YNL_PSP_H__ */
